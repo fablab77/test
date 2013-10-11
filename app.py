292b3e3c5bec3c1b17afa6bc103ac54ce78d7931
Sergey Soloviov <thatmean@gmail.com>
2013-10-11 18:14:04 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from flask import Flask
+# from flask.ext.heroku import Heroku
+from flask import render_template
+from jinja2.exceptions import TemplateNotFound
+app = Flask(__name__)
+# heroku = Heroku(app)
+app.jinja_env.add_extension('jinja2htmlcompress.SelectiveHTMLCompress')
+
+@app.route('/')
+@app.route('/<page>/')
+def any_page(page='index'):
+    try:
+        return render_template(page + '.html')
+    except TemplateNotFound:
+        return render_template('404.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
---
+++ static/js/all.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	
+
+	$('#filter-1 a').click(function (e) {
+		e.preventDefault()
+		$(this).tab('show')
+	})
+
+
+
+});
