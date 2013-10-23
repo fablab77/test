ed8195174584f71b490624a298011e7f5e1f6294
Eugen Soloviov <suenot@gmail.com>
2013-10-23 14:58:54 +0400

    fixs

--- static/js/all.js
+++ static/js/all.js
@@ -1,17 +1,25 @@
 $(document).ready(function(){
-	
 
-	$('#filter-1 a').click(function (e) {
-		e.preventDefault()
-		$(this).tab('show')
-	})
-
-
-
-
-	$("input[type=file]").nicefileinput();
-
-
-
+    'use strict';
+    // Change this to the location of your server-side upload handler:
+    var url = window.location.hostname === 'blueimp.github.io' ?
+                '//jquery-file-upload.appspot.com/' : 'server/php/';
+    $('#fileupload').fileupload({
+        url: url,
+        dataType: 'json',
+        done: function (e, data) {
+            $.each(data.result.files, function (index, file) {
+                $('<p/>').text(file.name).appendTo('#files');
+            });
+        },
+        progressall: function (e, data) {
+            var progress = parseInt(data.loaded / data.total * 100, 10);
+            $('#progress .progress-bar').css(
+                'width',
+                progress + '%'
+            );
+        }
+    }).prop('disabled', !$.support.fileInput)
+        .parent().addClass($.support.fileInput ? undefined : 'disabled');
 
 });
